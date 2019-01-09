0806f7a3b6f4cdbd1ed6b88ad2a9dfd988782855
Patricio de la Guardia <padlgt01@gmail.com>
2019-01-09 09:11:34 -0500

    Add start script

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const axios = require('axios');
 const app = express();
 
 //middleware function that will return static assets for every request made to the server
+//It would be great if a caching solution was implemented so that the assets do not get sent back on each request.
 app.use(express.static(`${__dirname}/client/build`));
 
+//Handler that will get all characters from the swapi api. Will be called from the 'App' component during componentWillMount lifecycle method
 app.get('/characters', async (req, res) => {
   let {data: characters} = await axios.get('https://swapi.co/api/people');
   characters = characters.results.map(char => {
@@ -16,6 +18,7 @@ app.get('/characters', async (req, res) => {
   res.json(characters);
 })
 
+//Handler that will get a character based on a character id. Will be called from the 'Character' component duringcomponentWillMount lifecycle method
 app.get('/characters/:id', async (req, res) => {
   const id = req.params.id;
   let {data: character} = await axios.get('https://swapi.co/api/people/'+id);
